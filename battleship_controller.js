c48e980e931f65f959068a380e70d47b15e483b9
ryatomaverun <ryatomaverun@learnacademy.org>
2016-11-23 11:37:40 -0800

    second commit

---
+++ battleship_controller.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  // when user clicks on cell changes to clicked class
+  $("td").on("click", function(){
+    $(this).addClass("clicked");
+
+  });
+
+
+});
--- battleship_model.js
+++ battleship_model.js
@@ -1,11 +1,17 @@
-//create 10x10 board with individual cell ids and row ids
-//first for loop creates 10 rows in table
-for (var row = 0; row < 10; row++) {
-  //add row with individual id to table
-  $("table").append('<tr id="' + row + '"></tr>');
-  //second for loop creates 10 cells in each row
-  for (var cell = 0; cell < 10; cell++) {
-    //add cell with individual id to each row
-    $("#" + row).append('<td id="' + row + cell + '"></td>');
+$(document).ready(function() {
+
+
+  //create 10x10 board with individual cell ids and row ids
+  //first for loop creates 10 rows in table
+  for (var row = 0; row < 10; row++) {
+    //add row with individual id to table
+    $("table").append('<tr id="' + row + '"></tr>');
+    //second for loop creates 10 cells in each row
+    for (var cell = 0; cell < 10; cell++) {
+      //add cell with individual id to each row
+      $("#" + row).append('<td id="' + row + cell + '"></td>');
+    }
   }
-}
+
+
+});
