354cf3c4b6a7777703a956bad0293608b26bd5d1
ryatomaverun <ryatomaverun@learnacademy.org>
2016-11-23 11:59:35 -0800

    third commit

--- battleship_controller.js
+++ battleship_controller.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // when user clicks on cell changes to clicked class
   $("td").on("click", function(){
     $(this).addClass("clicked");
-
+    counter();
   });
 
 
--- battleship_model.js
+++ battleship_model.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-
   //create 10x10 board with individual cell ids and row ids
   //first for loop creates 10 rows in table
   for (var row = 0; row < 10; row++) {
@@ -14,4 +13,12 @@ $(document).ready(function() {
   }
 
 
+
 });
+
+var missiles = 25;
+
+function counter() {
+  console.log("You have " + missiles + " move(s) left.");
+  missiles--;
+}
